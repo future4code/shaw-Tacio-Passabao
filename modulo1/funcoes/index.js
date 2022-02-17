cff52815ee9caf1417cbb0b99ebb392b93ed6b25
Tácio Franco <tacio.francosjdr@gmail.com>
2022-02-17 15:17:34 -0300

    Aula sobre funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,97 @@
+//Interpretação de Código 1
+
+//a) Será impresso 10 e 50 no console, respectivamente
+
+//b) A função seria invocada, calcularia o resultado e armazenaria na memória do programa,
+//   sem imprimir nada no console, uma vez que o comando .log não foi dado.
+
+
+//Interpretação de Código 2
+
+//a) O código lê a frase do usuário, armazena tudo em lower case, e se incluir a palavra
+//   "cenoura" após a conversão, te retorna true se houve a palavra na frase, te retorna false
+//   se não houver a palavra.
+
+// b) i. true ii. true iii. true - iii Retorna true pelo fato de "cenouras" conter "cenoura"
+
+
+//Escrita de Código 1
+
+
+// a. 
+    // let informacao = function(nome, idade, casa, profissao){
+    //     console.log(`Oi, eu sou o ${nome}, tenho ${idade} anos, moro em ${casa}, e sou ${profissao}`);
+    // }
+
+    // informacao("Caio", "23", "São Paulo", "Estudante")
+
+//b.
+    // let informacao = function(nome, idade, casa, profissao){
+    //     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${casa} e sou ${profissao}`)
+    // }
+    // informacao(prompt("Digite seu nome"), prompt("Digite sua idade"), prompt("Digite seu endereço"), prompt("Digite sua profissao"))
+
+
+//Escrita de Código 2
+
+    //a.
+        // let calculo = function(num1, num2){
+        //     let soma = num1 + num2
+        //     console.log(`A soma de ${num1} com ${num2} é:`,soma)
+        //     return soma
+        // }
+        // calculo((Number(prompt("Digite o primeiro número"))), Number((prompt("Digite o segundo número"))))
+
+    //b.
+        // let comparacao = function(num1, num2){
+        //     let comparacao = num1 >= num2
+        //     console.log(comparacao)
+        //     return comparacao
+        // }
+        // comparacao(Number(prompt("Digite um número aqui")), Number(prompt("Digite outro número aqui")))
+
+    //c.
+        // let parOuImpar = function(num1){
+        //     let comparacao = (num1 % 2) ===0
+        //     console.log(comparacao)
+        //     return comparacao
+        
+        // }
+        // parOuImpar(Number(prompt("Digite aqui um número")))
+
+    //d.
+    //     let frases = function(frase){
+    //         let tamanhoFrase = (prompt("Digite aqui uma frase"))
+    //         console.log(tamanhoFrase + " " + tamanhoFrase.toUpperCase() + " " + tamanhoFrase.length)
+    //     }
+    // frases()
+
+
+//Escrita de Código 3
+    // let num1 = Number((prompt("Digite aqui um número")))
+    // let num2 = Number((prompt("Digite aqui outro número.")))
+    // let soma = function(num1,num2){
+    //     let somar = num1 + num2
+    //     console.log(`O resultado da soma é ${somar}`);
+        
+    // }
+    // let diferenca = function(num1, num2){
+    //     let diferenca = num1 - num2
+    //     console.log(`O resultado da diferença é ${diferenca}`)
+        
+    // }
+    // let multiplicacao = function(num1, num2){
+    //     let multiplicacao = num1*num2
+    //     console.log(`O resultado da multiplicação é ${multiplicacao}`);
+        
+    // }
+    // let divisao = function(num1, num2){
+    //     let divisao = num1 / num2
+    //     console.log(`O resultado da divisão é ${divisao}`)
+        
+    // }
+    // soma(num1, num2)
+    // diferenca(num1, num2)
+    // multiplicacao(num1, num2)
+    // divisao(num1, num2)
+
