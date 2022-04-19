9644ae4a58fe8bd5954ba3b51eadac4f816bbde9
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-04-19 16:47:00 -0300

    Instalab: Start (Empty)

--- modulo3/use-state/instalab/src/App.js
+++ modulo3/use-state/instalab/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Post from './components/Post/Post';
+import styled from "styled-components"
 
-function App() {
+const AppContainer = styled.div `
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Post
+        nomeUsuario={'Usuário'}
+        fotoUsuario={'https://picsum.photos/50/50'}
+        fotoPost={'https://picsum.photos/200/150'}
+      />
+    </AppContainer>
   );
 }
+
 
 export default App;
