8f2174a919c0f7bbb3f385437475f7e1269293d6
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-04-22 16:35:13 -0300

    Modulo 3: Aula 32 - PokeCard

--- modulo3/aula32-use-effect/src/App.js
+++ modulo3/aula32-use-effect/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./styles.css";
+import axios from "axios";
+import PokeCard from "/src/Components/PokeCard.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+
+  const [pokeName, setPokeName] = useState("");
+  const [pokeList, setPokeList] = useState([]);
+
+  useEffect(() => getPokemon(), [])
+
+  const getPokemon = async () => {
+    const url = "https://labenu-cors-proxy.herokuapp.com/?url=https://pokeapi.co/api/v2/pokemon/?limit=151"
+    
+    try {
+      const response = await axios.get(url)
+      setPokeList(response.data.results)
+      console.log(response.data.results)
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  const changePokeName = event => {
+    setPokeName(event.target.value);
+  };
+
+    return (
+      <div className="App">
+        <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
+      </div>
+    );
+  }
 
 export default App;
