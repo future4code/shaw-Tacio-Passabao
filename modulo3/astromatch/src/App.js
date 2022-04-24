4b113fc618e0a7fe2a21edbfdcc49a58789898c6
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-04-24 18:27:29 -0300

    Astromatch quase completo, termino amanhã

--- modulo3/astromatch/src/App.js
+++ modulo3/astromatch/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Home from "./Components/home";
+import Matches from "./Components/match";
+import { GlobalStyle } from "./styled-app";
+import { Header, HermeneImg, KeyboardKeys, Main } from "./appStyle";
+import HermeneIcon from './assets/imgs/two-hearts.png';
+import arrowLeftIcon from "./assets/imgs/arrow-left.svg";
+import arrowRightIcon from "./assets/imgs/arrow-right.svg";
 
 function App() {
+
+  const [currentScreen, setCurrentScreen] = useState("home");
+  const [profile, setProfile] = useState([]);
+
+  const saveProfile = (data) => {
+    setProfile(data)
+  };
+
+  const changeToHome = () => {
+    setCurrentScreen("home");
+  };
+
+  const changeToMatches = () => {
+    setCurrentScreen("matches");
+  };
+
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "home":
+        return <Home
+        changeToMatches={changeToMatches}
+        changeToHome={changeToHome}
+        profile={profile}
+        saveProfile={saveProfile}
+        />
+      case "matches":
+        return <Matches changeToHome={changeToHome} 
+        saveProfile={saveProfile}
+        />
+      default:
+        return <Home 
+        changeToMatches={changeToMatches} 
+        changeToHome={changeToHome}
+        profile={profile}
+        saveProfile={saveProfile}
+        />
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <GlobalStyle />
+      <Header>
+        <HermeneImg src={HermeneIcon} alt="Ícone HermeneMatch" />
+        HermeneMatch
+        <HermeneImg src={HermeneIcon} alt="Ícone HermeneMatch" />
+      </Header>
+      <KeyboardKeys>
+        <img src={arrowLeftIcon} alt="Ícone da seta esquerda do teclado" />
+        <img src={arrowRightIcon} alt="Ícone da seta direita do teclado" />
+        <h3>ESPAÇO</h3>
+        <h3>M</h3>
+        <h3>BACKSPACE</h3>
+        <p>Recusar perfil</p>
+        <p>Curtir</p>
+        <p> Deletar todos os matches </p>
+        <p>Matches</p>
+        <p>Voltar</p>
+        </KeyboardKeys>
+        {renderScreen()}
+    </Main>
   );
-}
+};
 
 export default App;
