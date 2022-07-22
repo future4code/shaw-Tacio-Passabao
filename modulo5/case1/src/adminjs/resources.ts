1e441906a6b13c418da6c7ff0229192e1a7b679b
Tácio <tacio.francosjdr@gmail.com>
2022-07-22 17:40:24 -0300

    Ongoing: Case1

--- modulo5/case1/src/adminjs/resources.ts
+++ modulo5/case1/src/adminjs/resources.ts
@@ -1,10 +1,16 @@
+
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../models";
+import { Category, Course } from "../models";
 import { categoryResourceOptions } from "./resources/category";
+import { courseResourceOptions } from "./resources/course";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
+    resource: Course,
+    options: courseResourceOptions
+  },
+  {
     resource: Category,
     options: categoryResourceOptions
-  }
+  },
 ]
