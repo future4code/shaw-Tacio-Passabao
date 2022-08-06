2b449d330640d86ef6885330a4000417c93bb042
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-08-05 23:31:45 -0300

    WIP: Spotify Clone

---
+++ modulo5/Case3 - Spotify/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "../components/Sidebar";
+import Center from "../components/Center";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        <Sidebar />
+        <Center />
+      </main>
+      <div>{/*player*/}</div>
+    </div>
+  );
+};
+
+export default Home;
