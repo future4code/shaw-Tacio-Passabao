695a9596ef3b3b462755161f8900e214ca67fb92
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-08-12 23:12:12 -0300

    Ongoing: Twitter Clone

---
+++ modulo5/Case4 - Twitter Clone/components/TweetBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TweetBox() {
+  return (
+    <div>
+      <img className=" h-14 w-14 rounded-full object-cover mt-4"src="https://links.papareact.com/gll" alt="" />
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default TweetBox
